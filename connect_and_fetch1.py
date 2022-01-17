52cec785db28ddc653f90cc072834a7eb21f308d
Henry Booth <boothh3@cardiff.ac.uk>
2022-01-17 17:16:03 +0000

    multivariate model working

---
+++ connect_and_fetch1.py
@@ -0,0 +1,21 @@
+import psycopg2
+import pandas as pd
+
+
+def connect_and_fetch():
+
+   #establishing the connection
+   conn = psycopg2.connect(
+      database="fear_and_greed", user='postgres', ***REMOVED***='***REMOVED***', host='127.0.0.1', port= '5432'
+   )
+   sql_query = pd.read_sql_query('''
+                                  SELECT
+                                  *
+                                  FROM fear_greed_index
+                                  ''', conn)
+
+   df = pd.DataFrame(sql_query, columns=['timestamp', 'value', 'price', 'value_classification'])
+
+   #Closing the connection
+   conn.close()
+   return df
